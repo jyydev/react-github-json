c6e98185828363f9cce5f787dba234d314a682bd
JY <84664024+jyydev@users.noreply.github.com>
2021-06-25 22:52:16 +0800

    t

--- src/pages/Home/components/Edit.js
+++ src/pages/Home/components/Edit.js
@@ -37,14 +37,14 @@ const Edit = ({ add, submittingStatus }) => {
 
   return (
     <div>
-      <h1>備忘錄</h1>
+      <h1>備忘錄 (Github API -&gt; GET, PUT -&gt; JSON file)</h1>
       <p>記事：</p>
-      <input type="text" value={note} onChange={noteChange} />
+      <input type='text' value={note} onChange={noteChange} />
       <p>日期：</p>
-      <input type="date" value={date} onChange={dateChange} />
+      <input type='date' value={date} onChange={dateChange} />
       <p>時間：</p>
-      <input type="time" value={time} onChange={timeChange} />
-      <button onClick={addItem} className="add">
+      <input type='time' value={time} onChange={timeChange} />
+      <button onClick={addItem} className='add'>
         新增
       </button>
     </div>
